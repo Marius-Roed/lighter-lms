ef8044538dc38fcfe0f29f7ab28d6a20bd1b5270
Marius-Roed <mwroed@gmail.com>
2025-11-05 12:59:09 +0100

    fix: course admin pagination

--- assets/svelte/src/components/list-table/body-row/TitleCell.svelte
+++ assets/svelte/src/components/list-table/body-row/TitleCell.svelte
@@ -9,6 +9,12 @@
             href={`/wp-admin/post.php?post=${post.id}&action=edit`}
         >
             {post.course_title ?? post.title ?? "Untitled"}
+            {#if post.status != "publish"}
+                <span
+                    style="color:#000;display:inline-block;text-transform:capitalize;"
+                    >&ndash; {post.status}</span
+                >
+            {/if}
         </a>
     </strong>
 </td>
